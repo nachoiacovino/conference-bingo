0c931af34d55303fd2e4c392a2515fe30bd4694c
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-11-12 01:14:06 +0100

    Add default texts and toggle 'active' with click

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,40 +1,71 @@
 import './Board.scss';
 
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
 const Board = () => {
-  const data = [
-    { text: '(child noises in the background)', active: false },
-    { text: 'Hello, hello?', active: false },
-    { text: 'i need to jump in another call', active: false },
-    { text: 'can everyone go on mute', active: false },
-    { text: 'could you please get closer to the mic', active: false },
-    { text: '(loud painful echo / feedback)', active: false },
-    { text: 'Next slide, please.', active: false },
-    { text: 'can we take this offline?', active: false },
-    { text: 'is ___ on the call?', active: false },
-    { text: 'Could you share this slides afterwards?', active: false },
-    { text: 'can somebody grant presenter rights?', active: false },
-    { text: 'can you email that to everyone?', active: false },
-    { text: 'CONF CALL BINGO', active: true },
-    { text: 'sorry, i had problems logging in', active: false },
-    { text: '(animal noises in the background)', active: false },
-    { text: "sorry, i didn't found the conference id", active: false },
-    { text: 'i was having connection issues', active: false },
-    { text: "i'll have to get back to you", active: false },
-    { text: 'who just joined?', active: false },
-    { text: 'sorry, something ___ with my calendar', active: false },
-    { text: 'do you see my screen?', active: false },
-    { text: "let's wait for ___!", active: false },
-    { text: 'You will send the minutes?', active: false },
-    { text: 'sorry, i was on mute', active: false },
-    { text: 'can you repeate, please?', active: false },
-  ];
+  const [data, setData] = useState([
+    { id: uuid(), text: '(child noises in the background)', active: false },
+    { id: uuid(), text: 'Hello, hello?', active: false },
+    { id: uuid(), text: 'i need to jump in another call', active: false },
+    { id: uuid(), text: 'can everyone go on mute', active: false },
+    {
+      id: uuid(),
+      text: 'could you please get closer to the mic',
+      active: false,
+    },
+    { id: uuid(), text: '(loud painful echo / feedback)', active: false },
+    { id: uuid(), text: 'Next slide, please.', active: false },
+    { id: uuid(), text: 'can we take this offline?', active: false },
+    { id: uuid(), text: 'is ___ on the call?', active: false },
+    {
+      id: uuid(),
+      text: 'Could you share this slides afterwards?',
+      active: false,
+    },
+    { id: uuid(), text: 'can somebody grant presenter rights?', active: false },
+    { id: uuid(), text: 'can you email that to everyone?', active: false },
+    { id: uuid(), text: 'CONF CALL BINGO', active: true },
+    { id: uuid(), text: 'sorry, i had problems logging in', active: false },
+    { id: uuid(), text: '(animal noises in the background)', active: false },
+    {
+      id: uuid(),
+      text: "sorry, i didn't found the conference id",
+      active: false,
+    },
+    { id: uuid(), text: 'i was having connection issues', active: false },
+    { id: uuid(), text: "i'll have to get back to you", active: false },
+    { id: uuid(), text: 'who just joined?', active: false },
+    {
+      id: uuid(),
+      text: 'sorry, something ___ with my calendar',
+      active: false,
+    },
+    { id: uuid(), text: 'do you see my screen?', active: false },
+    { id: uuid(), text: "let's wait for ___!", active: false },
+    { id: uuid(), text: 'You will send the minutes?', active: false },
+    { id: uuid(), text: 'sorry, i was on mute', active: false },
+    { id: uuid(), text: 'can you repeate, please?', active: false },
+  ]);
+
+  const handleClick = (id) => {
+    setData(
+      data.map((item) =>
+        item.id === id ? { ...item, active: !item.active } : item,
+      ),
+    );
+  };
 
   return (
-    <div class="Board">
+    <div className="Board">
       <div className="Board-wrapper">
         {data.map((item, i) => (
-          <div className="box">
-            {i + 1} {item.text}
+          <div
+            className={`box ${item.active && 'active'}`}
+            key={item.id}
+            onClick={() => handleClick(item.id)}
+          >
+            {/* {i + 1} */} {item.text}
           </div>
         ))}
       </div>
