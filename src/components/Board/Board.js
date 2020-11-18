546b95f80cc0ce3c5adac4e0c628f53db36c0b2d
Nacho Iacovino <nacho.iacovino@gmail.com>
2020-11-18 16:46:09 +0100

    Add function to randomize data

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -4,55 +4,65 @@ import ConfettiGenerator from 'confetti-js';
 import { useEffect, useState } from 'react';
 import { v4 as uuid } from 'uuid';
 
+const baseData = [
+  { id: uuid(), text: '(child noises in the background)', active: false },
+  { id: uuid(), text: 'Hello, hello?', active: false },
+  { id: uuid(), text: 'i need to jump in another call', active: false },
+  { id: uuid(), text: 'can everyone go on mute', active: false },
+  {
+    id: uuid(),
+    text: 'could you please get closer to the mic',
+    active: false,
+  },
+  { id: uuid(), text: '(loud painful echo / feedback)', active: false },
+  { id: uuid(), text: 'Next slide, please.', active: false },
+  { id: uuid(), text: 'can we take this offline?', active: false },
+  { id: uuid(), text: 'is ___ on the call?', active: false },
+  {
+    id: uuid(),
+    text: 'Could you share this slides afterwards?',
+    active: false,
+  },
+  { id: uuid(), text: 'can somebody grant presenter rights?', active: false },
+  { id: uuid(), text: 'can you email that to everyone?', active: false },
+  { id: uuid(), text: 'sorry, i had problems logging in', active: false },
+  { id: uuid(), text: '(animal noises in the background)', active: false },
+  {
+    id: uuid(),
+    text: "sorry, i didn't found the conference id",
+    active: false,
+  },
+  { id: uuid(), text: 'i was having connection issues', active: false },
+  { id: uuid(), text: "i'll have to get back to you", active: false },
+  { id: uuid(), text: 'who just joined?', active: false },
+  {
+    id: uuid(),
+    text: 'sorry, something ___ with my calendar',
+    active: false,
+  },
+  { id: uuid(), text: 'do you see my screen?', active: false },
+  { id: uuid(), text: "let's wait for ___!", active: false },
+  { id: uuid(), text: 'You will send the minutes?', active: false },
+  { id: uuid(), text: 'sorry, i was on mute', active: false },
+  { id: uuid(), text: 'can you repeate, please?', active: false },
+];
+
 const Board = () => {
-  const [data, setData] = useState([
-    { id: uuid(), text: '(child noises in the background)', active: false },
-    { id: uuid(), text: 'Hello, hello?', active: false },
-    { id: uuid(), text: 'i need to jump in another call', active: false },
-    { id: uuid(), text: 'can everyone go on mute', active: false },
-    {
-      id: uuid(),
-      text: 'could you please get closer to the mic',
-      active: false,
-    },
-    { id: uuid(), text: '(loud painful echo / feedback)', active: false },
-    { id: uuid(), text: 'Next slide, please.', active: false },
-    { id: uuid(), text: 'can we take this offline?', active: false },
-    { id: uuid(), text: 'is ___ on the call?', active: false },
-    {
-      id: uuid(),
-      text: 'Could you share this slides afterwards?',
-      active: false,
-    },
-    { id: uuid(), text: 'can somebody grant presenter rights?', active: false },
-    { id: uuid(), text: 'can you email that to everyone?', active: false },
-    { id: uuid(), text: 'sorry, i had problems logging in', active: false },
-    { id: uuid(), text: '(animal noises in the background)', active: false },
-    {
-      id: uuid(),
-      text: "sorry, i didn't found the conference id",
-      active: false,
-    },
-    { id: uuid(), text: 'i was having connection issues', active: false },
-    { id: uuid(), text: "i'll have to get back to you", active: false },
-    { id: uuid(), text: 'who just joined?', active: false },
-    {
-      id: uuid(),
-      text: 'sorry, something ___ with my calendar',
-      active: false,
-    },
-    { id: uuid(), text: 'do you see my screen?', active: false },
-    { id: uuid(), text: "let's wait for ___!", active: false },
-    { id: uuid(), text: 'You will send the minutes?', active: false },
-    { id: uuid(), text: 'sorry, i was on mute', active: false },
-    { id: uuid(), text: 'can you repeate, please?', active: false },
-  ]);
+  const [data, setData] = useState([]);
   const [indices, setIndices] = useState([12]);
   const [bingos, setBingos] = useState(0);
   const [lastIndex, setLastIndex] = useState(null);
   const [limit] = useState(5);
 
   useEffect(() => {
+    let newData = [...baseData];
+
+    const shuffleArray = (arr) =>
+      arr
+        .map((a) => [Math.random(), a])
+        .sort((a, b) => a[0] - b[0])
+        .map((a) => a[1]);
+
     const insert = (arr, index, newItem) => [
       // part of the array before the specified index
       ...arr.slice(0, index),
@@ -62,14 +72,15 @@ const Board = () => {
       ...arr.slice(index),
     ];
 
-    setData((d) =>
-      insert(d, (limit * limit) / 2, {
-        id: uuid(),
-        text: 'CONF CALL BINGO',
-        active: true,
-        center: true,
-      }),
-    );
+    newData = shuffleArray(newData);
+    newData = insert(newData, (limit * limit) / 2, {
+      id: uuid(),
+      text: 'CONF CALL BINGO',
+      active: true,
+      center: true,
+    });
+
+    setData(newData);
   }, [limit]);
 
   useEffect(() => {
@@ -169,7 +180,7 @@ const Board = () => {
             key={item.id}
             onClick={() => handleClick(item.id, index)}
           >
-            {/* {index} */} {item.text}
+            {item.text}
           </div>
         ))}
         {/* {bingos && <div>bingos: {bingos}</div>} */}
